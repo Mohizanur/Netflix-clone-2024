e065fa96482ab65a8a6c3340744345125a57eabf
Mohizanur <rco47840@gmail.com>
2025-01-14 23:37:04 +0300

    Enhanced Top Rated movies display �

--- netflix-final/src/utils/requests.js
+++ netflix-final/src/utils/requests.js
@@ -3,7 +3,7 @@ const API_KEY = "1cc28d7cb8202fa7566afa90c4a8b9f4";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   fetchTVShows: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
