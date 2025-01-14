2d2c9a2dc49e29b4cdfc52431da5936449150146
Mohizanur <rco47840@gmail.com>
2025-01-14 22:25:23 +0300

    Enhanced API configuration for faster loading �

--- netflix-final/src/utils/axios.js
+++ netflix-final/src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3", // Fixed 'baseURL' typo
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "1cc28d7cb8202fa7566afa90c4a8b9f4",
+  },
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
